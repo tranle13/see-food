a5bc83981b5434a6984209faca561a9dc060d9a2
sunnyle13 <4anythingbutwork@gmail.com>
2022-10-07 20:23:38 -0400

    Initialize project and add gitignore

---
+++ See Food/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
